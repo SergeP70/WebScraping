ccb0a2fd6787b23f7d55fd3bd16e7c9edb9ad3e2
sergeP70 <serge@pille.be>
2025-01-29 11:28:44 +0100

    finalising the send mail

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # WebScraping
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+import time
+from dotenv import load_dotenv
 
+load_dotenv()
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+MY_CONTEXT = ssl.create_default_context()
+PASSWORD = os.getenv("PASSWORD")
+SENDER = os.getenv("USERNAME")
+RECEIVER = os.getenv("USERNAME")
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -19,8 +30,19 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
-    print("Email was sent")
+def send_email(message):
+    host = HOST
+    port = int(PORT)
+    username = SENDER
+    password = PASSWORD
+    receiver = RECEIVER
+    context = MY_CONTEXT
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+    print("Mail was sent")
 
 def store(extracted):
     with open('data.txt', 'a') as file:
@@ -32,13 +54,15 @@ def read(extracted):
 
 
 if __name__ == '__main__':
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, a new event was found")
 
+        time.sleep(2)
